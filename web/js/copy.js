5f1e936c70523fd8d4a8a700142059403c84a06a
worldpeacezb <18253252665q>
2018-07-15 09:47:48 +0800

    phn

--- web/js/copy.js
+++ web/js/copy.js
@@ -8,34 +8,35 @@ function creat(t_s){
     div1.appendChild(div2);
     document.body.appendChild(div1);
 }
-
 //创建新的商品栏
 function creatdiv() {
     var surprise = document.getElementById('banner');
     surprise.innerHTML += "<div class=\"w3ls_w3l_banner_nav_right_grid1\">\n" +
-        "\t\t\t\t\t<div class=\"col-md-3 w3ls_w3l_banner_left\">\n" +
-        "\t\t\t\t\t\t<div class=\"hover14 column\">\n" +
-        "\t\t\t\t\t\t<div class=\"agile_top_brand_left_grid w3l_agile_top_brand_left_grid\">\n" +
-        "\t\t\t\t\t\t\t<div class=\"agile_top_brand_left_grid1\">\n" +
-        "\t\t\t\t\t\t\t\t<figure>\n" +
-        "\t\t\t\t\t\t\t\t\t<div class=\"snipcart-item block\">\n" +
-        "\t\t\t\t\t\t\t\t\t\t<div class=\"snipcart-thumb\">\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<a href=\"single.html\"><img src=\"src/images/4.jpg\" alt=\" \" class=\"img-responsive\" /></a>\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<p>dishwash gel, lemon  (1.5 ltr)</p>\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<h4>$8.00</h4>\n" +
-        "\t\t\t\t\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t\t\t\t\t\t<div class=\"snipcart-details\">\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<button class=\"btn btn-danger my-cart-btn hvr-sweep-to-right\" id=\"17\" data-name=\"Dishwash gel, lemon\" data-summary=\"summary 17\" data-price=\"8.00\" data-quantity=\"1\" data-image=\"images/17.png\">购买</button>\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<button class=\"btn btn1-danger my-cart-btn1 hvr-sweep-to-right\" id=\"collect\" onclick=\"collect()\t\" >收藏</button>\n" +
-        "\t\t\t\t\t\t\t\t\t\t\t<button class=\"btn btn-danger my-cart-btn2 \" style=\"margin-left: 10%\" id=\"no_collect\" onclick=\"no_collect()\">取消收藏</button>\n" +
-        "\t\t\t\t\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t\t\t\t</figure>\n" +
-        "\t\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t\t</div>\n" +
-        "\t\t\t\t\t</div>\n" +
-        "\t\t\t\t</div>";
+        "<div class=\"col-md-3 w3ls_w3l_banner_left\">\n" +
+        "<div class=\"hover14 column\">\n" +
+        "<div class=\"agile_top_brand_left_grid w3l_agile_top_brand_left_grid\">\n" +
+        "<div class=\"agile_top_brand_left_grid1\">\n" +
+        "\<figure>\n" +
+        "<div class=\"snipcart-item block\">\n" +
+        "<div class=\"snipcart-thumb\">\n" +
+        "<a href=\"single.html\"><img src=\"src/images/4.jpg\" alt=\" \" class=\"img-responsive\" /></a>\n" +
+        "<p>dishwash gel, lemon  (1.5 ltr)</p>\n" +
+        "<h4>$8.00</h4>\n" +
+        "</div>\n" +
+        "<div class=\"snipcart-details\">\n" +
+        "<button class=\"btn btn-danger my-cart-btn hvr-sweep-to-right\" id=\"17\" data-name=\"Dishwash gel, lemon\" data-summary=\"summary 17\" data-price=\"8.00\" data-quantity=\"1\" data-image=\"images/17.png\">购买</button>\n" +
+        "<button class=\"btn btn1-danger my-cart-btn1 hvr-sweep-to-right\" id=\"collect\" onclick=\"collect()\t\" >收藏</button>\n" +
+        "<button class=\"btn btn-danger my-cart-btn2 \" style=\"margin-left: 10%\" id=\"no_collect\" onclick=\"no_collect()\">取消收藏</button>\n" +
+        "</div>\n" +
+        "</div>\n" +
+        "</figure>\n" +
+        "</div>\n" +
+        "</div>\n" +
+        "</div>\n" +
+        "</div>\n" +
+        "</div>";
+
+}
     /*
 //4
     //var div1 = document.getElementById('one');
@@ -98,7 +99,6 @@ function creatdiv() {
 
 
 
-}
 
 /*for (var i = 0; i < 4; i++)
 {
